29f3fbf2ffcbf53c94c135d8e6ce2e3b6caade0d
SAMIAN <110727895+SAMIAN04@users.noreply.github.com>
2024-01-27 05:44:04 +0000

    Add files via upload
    Undefined bug fixed

--- qr.js
+++ qr.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById(`btn`);
 const qr = document.getElementById(`qr`);
-const input = document.getElementById(`input`);
+const input = document.getElementById(`input`).value;
 const qrImg = document.getElementById(`qrimg`);
 const hy = document.getElementById(`hy`)
 btn.addEventListener(`click`, generate);
@@ -8,7 +8,7 @@ btn.addEventListener(`click`, generate);
 function generate() {
     if (input.value != ``) {
        hy.innerHTML =  ''
-        qrImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data= ${input.Value}`
+      qrImg.src= `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`
     }
     else{
         hy.innerHTML = `enter some text` 
@@ -16,3 +16,4 @@ function generate() {
     }
    
 }
+console.log(input.value);
